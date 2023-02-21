60c64e5426a664e781c89686c6ae0b80e01732ad
omarovic555 <omarhachicha111@gmail.com>
2023-02-21 15:37:28 +0100

    Create telegrambot.py

---
+++ app/telegrambotApp.py
@@ -0,0 +1,46 @@
+
+import telebot
+from facebook_scraper import get_posts
+from loguru import logger
+
+
+TOKEN = '6162231182:AAEeKmw5nOH39NsKzQfy3PB9i539xVTm0BQ'
+BOT_CHAT_ID = "6162231182"
+CHAT_ID = "************"
+
+bot = telebot.TeleBot(TOKEN)
+x = bot.get_me()
+
+@bot.message_handler(func=lambda message: message.text)
+def handle_number(message):
+    """
+    Handle messages containing a Facebook page name.
+    Scrape the page for the first 3 sections and send the posts data back to the user.
+
+    Args:
+        message (telebot.types.Message): The message object containing the Facebook page name.
+
+    Returns:
+        None.
+    """
+    posts: list = []
+
+    # Get the first 3 sections from a Facebook page
+    for post in get_posts(message.text, pages=3):
+        post_data = {
+            "post_id": post['post_id'],
+            "time": post['time'],
+            "text": post['text'][:199],
+            "likes": post['likes'],
+            "shares": post['shares'],
+            "comments": post['comments']
+        }
+        posts.append(post_data)
+        logger.info("PostId:" + post['post_id'])
+
+    # Send the result back to the user
+    bot.send_message(CHAT_ID,str(posts))
+
+# Start the bot
+bot.polling()
+
